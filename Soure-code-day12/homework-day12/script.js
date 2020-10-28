f672fbcab524c7de1110e6712780d4beb39ab084
dohuyhoangf <dohuyhoangf@gmail.com>
2020-10-29 02:54:04 +0700

    hw12

---
+++ Soure-code-day12/homework-day12/script.js
@@ -0,0 +1,18 @@
+document.write('Bài 1. Cho một mảng số nguyên. Viết hàm kiểm tra nếu trong mảng có phần tử âm thì thay thế nó bằng 0. <br/>');
+document.write('Câu lệnh để gọi hàm là checkNumber() <br/>');
+document.write('Bài 2. Viết hàm tìm số nhỏ nhất và lớn nhất trong mảng số nguyên. <br/>');
+document.write('Câu lệnh để gọi hàm là minMax() <br/>');
+document.write('Viết hàm trả về vị trí đầu tiên của số lẻ, vị trí cuối cùng của số chẵn trong mảng bất kì.<br/>')
+document.write('Câu lệnh để gọi hàm là <br/>');
+
+
+// function checkNumber(oldArr){
+//     const newArr = oldArr.map(x => x == 0);
+//     console.log(newArr);
+// }
+
+function minMax(numbers){
+    const minNumber = Math.min.apply(null, numbers);
+    const maxNumber = Math.max.apply(null, numbers);
+    console.log("Số lớn nhất là "+maxNumber+" và số nhỏ nhất là " +minNumber+ "");
+}
--- Soure-code-day12/homework-day12/script12.js
+++
@@ -1,4 +0,0 @@
-function checkNumber(oldArr){
-    const newArr = oldArr.map(x<0 => x == 0);
-    console.log(newArr);
-}
